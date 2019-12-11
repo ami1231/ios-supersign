2e3e05abd9259c216a6832c303e5326b28293445
Peter.Hong <peter5645@hotmail.com>
2019-12-11 22:27:29 +0800

    先commit

--- ios-supersign-service/src/main/java/com/naoh/iossupersign/thridparty/appleapi/AppleApiService.java
+++ ios-supersign-service/src/main/java/com/naoh/iossupersign/thridparty/appleapi/AppleApiService.java
@@ -5,10 +5,13 @@ import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
 import com.naoh.iossupersign.enums.AppleApiEnum;
 import com.naoh.iossupersign.model.bo.AuthorizeBO;
+import com.naoh.iossupersign.model.po.ApplePO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.io.File;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
@@ -31,7 +34,7 @@ public class AppleApiService extends AppleApi{
         String url = AppleApiEnum.LIST_DEVICE_API.getApiPath();
 
 
-        restTemplate.exchange(url,AppleApiEnum.LIST_DEVICE_API.getHttpMethod(),)
+//        restTemplate.exchange(url,AppleApiEnum.LIST_DEVICE_API.getHttpMethod(),)
 
 
         String result = HttpRequest.get(url).
@@ -87,7 +90,78 @@ public class AppleApiService extends AppleApi{
     }
 
 
+    public File getMobileprovision(ApplePO applePO, String devId){
 
+        Map<String, Object> requestData = new HashMap<>();
+
+        Map<String, Object> profileCreateRequest = new HashMap<>();
+        profileCreateRequest.put("attributes", getAttributes());
+        profileCreateRequest.put("relationships", getRelationships(applePO, devId));
+        profileCreateRequest.put("type", "profiles");
+
+        requestData.put("data", profileCreateRequest);
+
+        String url = AppleApiEnum.CREATE_PROFILE_API.getApiPath();
+        restTemplate.exchange(url, AppleApiEnum.CREATE_PROFILE_API.getHttpMethod(), )
+
+        File file = null;
+        return file;
+    }
+
+    private Map<String, Object> getAttributes(){
+        Map<String, Object> attributes = new HashMap<>();
+        attributes.put("name", "Mobileprovision123");
+        attributes.put("profileType", "IOS_APP_DEVELOPMENT");
+        return attributes;
+    }
+
+    private Map<String, Object> getRelationships(ApplePO applePO, String devId){
+        Map<String, Object> relationships = new HashMap<>();
+        relationships.put("bundleId", getBundleId(applePO));
+        relationships.put("certificates", getCertificates(applePO));
+        relationships.put("devices", getDevices(devId));
+        return relationships;
+    }
+
+    private Map<String, Object> getBundleId(ApplePO applePO){
+        Map<String, Object> bundleId = new HashMap<>();
+
+        Map<String, Object> data = new HashMap<>();
+        data.put("id", applePO.getBundleIds());
+        data.put("type", "bundleIds");
+
+        bundleId.put("data", data);
+
+        return bundleId;
+    }
+
+    private Map<String, Object> getCertificates(ApplePO applePO){
+        Map<String, Object> certificates = new HashMap<>();
+        List<Map<String, Object>> list = new ArrayList<>();
+
+        Map<String, Object> data = new HashMap<>();
+        data.put("id", applePO.getCerId());
+        data.put("type", "certificates");
+
+        list.add(data);
+
+        certificates.put("data", list);
+        return certificates;
+    }
+
+    private Map<String, Object> getDevices(String devId){
+        Map<String, Object> devices = new HashMap<>();
+        List<Map<String, Object>> list = new ArrayList<>();
+
+        Map<String, Object> data = new HashMap<>();
+        data.put("id", devId);
+        data.put("type", "devices");
+
+        list.add(data);
+        devices.put("data", list);
+
+        return devices;
+    }
 
 
 }
