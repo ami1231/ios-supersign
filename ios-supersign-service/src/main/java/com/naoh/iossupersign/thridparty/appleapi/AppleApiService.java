bb49e2ba43b70048a8784714186c092f7e6bf331
Ami.Tsai <ami.tsai@tung-shen.com>
2019-12-11 22:42:35 +0800

    增加body機制

--- ios-supersign-service/src/main/java/com/naoh/iossupersign/thridparty/appleapi/AppleApiService.java
+++ ios-supersign-service/src/main/java/com/naoh/iossupersign/thridparty/appleapi/AppleApiService.java
@@ -6,6 +6,7 @@ import com.naoh.iossupersign.enums.AppleApiEnum;
 import com.naoh.iossupersign.model.bo.AuthorizeBO;
 import com.naoh.iossupersign.model.dto.AppleApiResult;
 import com.naoh.iossupersign.model.dto.AppleDeviceDTO;
+import com.naoh.iossupersign.model.dto.AppleReqBody;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
@@ -38,24 +39,19 @@ public class AppleApiService extends AppleApi{
         return responseBody.getData();
     }
 
-    public String addDeviceUdidToAppleAccount(String udid , AuthorizeBO authorizeBO){
-        Map<String,String> body = new HashMap<>();
-        body.put("type", "devices");
-        Map attributes = new HashMap();
-        attributes.put("name", udid);
-        attributes.put("udid", udid);
-        attributes.put("platform", "IOS");
-//        body.put("attributes", attributes);
-        Map data = new HashMap();
-        data.put("data", body);
-        String url = AppleApiEnum.REGISTER_NEW_DEVICE_API.getApiPath();
-//        String result = HttpRequest.post(url).
-//                addHeaders(getToken(authorizeBO.getP8(), authorizeBO.getIss(), authorizeBO.getKid())).
-//                body(JSON.toJSONString(data)).execute().body();
-//        Map map = JSON.parseObject(result, Map.class);
-//        Map data1 = (Map) map.get("data");
-//        String id = (String)data1.get("id");
-        return "";
+    public String registerNewDevice(String udid , AuthorizeBO authorizeBO){
+        HttpHeaders headers = getToken(authorizeBO.getP8(), authorizeBO.getIss(), authorizeBO.getKid());
+
+        AppleReqBody attributes = AppleReqBody.init().add("name", udid).add("udid", udid).add("platform", "IOS");
+        AppleReqBody body = AppleReqBody.init().add("type", "devices").add("attributes", attributes);
+        AppleReqBody data = AppleReqBody.init().add("data",body);
+        HttpEntity<Map<String,Object>> httpEntity = new HttpEntity<>(data,headers);
+        String url = AppleApiEnum.LIST_DEVICE_API.getApiPath();
+
+        ResponseEntity<String> response = restTemplate.exchange(url,AppleApiEnum.LIST_DEVICE_API.getHttpMethod(),httpEntity,
+                String.class);
+        System.out.println(response.getBody());
+        return response.getBody();
     }
 
 
